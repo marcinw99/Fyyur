041dc6a1eeecc56143ed52edf90d61d471746d68
Marcin Wąsik <marcin.wasik@ridedott.com>
2022-05-31 18:51:27 +0200

    feat: complete Venue model and migrate

--- app.py
+++ app.py
@@ -34,13 +34,18 @@ class Venue(db.Model):
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False, default=[]) # I wanted to use an array of enums but it is not well documented, relying on form validation
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    website = db.Column(db.String(120), nullable=True)
+    facebook_link = db.Column(db.String(120), nullable=True)
+    seeking_talent = db.Column(db.Boolean(), nullable=False)
+    seeking_description = db.Column(db.String(500), nullable=True)
+
     shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
