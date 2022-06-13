77feaacfa88a415667765220b05c2693602b0a76
Marcin Wąsik <marcin.wasik@ridedott.com>
2022-06-13 11:45:49 +0200

    feat: handle artist page endpoint

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 def get_venue_page_payload(venue, past_shows, upcoming_shows):
     past_shows_length = len(past_shows)
     upcoming_shows_length = len(upcoming_shows)
@@ -24,6 +23,37 @@ def get_venue_page_payload(venue, past_shows, upcoming_shows):
         'facebook_link': venue.facebook_link,
         'seeking_talent': venue.seeking_talent,
         'seeking_description': venue.seeking_description,
+        "past_shows": formatted_past_shows,
+        "upcoming_shows": formatted_upcoming_shows,
+        "past_shows_count": past_shows_length,
+        "upcoming_shows_count": upcoming_shows_length,
+    }
+
+
+def get_artist_page_payload(artist, past_shows, upcoming_shows):
+    past_shows_length = len(past_shows)
+    upcoming_shows_length = len(upcoming_shows)
+    formatted_past_shows = []
+    formatted_upcoming_shows = []
+
+    if past_shows_length > 0:
+        formatted_past_shows = [show.format() for show in past_shows]
+
+    if upcoming_shows_length > 0:
+        formatted_upcoming_shows = [show.format() for show in upcoming_shows]
+
+    return {
+        'id': artist.id,
+        'name': artist.name,
+        'genres': artist.genres,
+        'city': artist.city,
+        'state': artist.state,
+        'phone': artist.phone,
+        'image_link': artist.image_link,
+        'website_link': artist.website_link,
+        'facebook_link': artist.facebook_link,
+        'seeking_venue': artist.seeking_venue,
+        'seeking_description': artist.seeking_description,
         "past_shows": formatted_past_shows,
         "upcoming_shows": formatted_upcoming_shows,
         "past_shows_count": past_shows_length,
